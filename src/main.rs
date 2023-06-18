82265b65f9b909179507f8b06e5f362b9a22ee6a
Billy Richardson <contact@richardson.dev>
2023-06-18 02:40:07 -0700

    Refactor retries

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,64 @@
-use hex;
-use std::env;
 use rand::Rng;
 use sha2::{Sha512, Digest};
-use reqwest;
-use serde_json::json;
+use reqwest::{Client, Url};
+use std::env;
+use std::net::Ipv4Addr;
 use std::time::Duration;
 use tokio::time::sleep;
-use std::net::Ipv4Addr;
-use reqwest::Error;
-use std::collections::HashMap;
+use serde_json::Value;
+use std::future::Future;
+
+const HEX_CHARS: &[u8] = b"abcdef0123456789";
+const RETRY_DELAY: u64 = 2;
 
 fn random_hex(len: usize) -> String {
     let mut rng = rand::thread_rng();
-    let chars: Vec<char> = "abcdef0123456789".chars().collect();
-    (0..len).map(|_| chars[rng.gen_range(0..chars.len())]).collect()
+    (0..len).map(|_| HEX_CHARS[rng.gen_range(0..HEX_CHARS.len())] as char).collect()
 }
 
-async fn get_ip(client: &reqwest::Client, host: &String) -> Result<String, Box<dyn std::error::Error>> {
-    for i in 0..5 {
-        let response = client.get(format!("https://{}/ip.txt", host)).send().await?;
+async fn retry<F, Fut, T, E>(mut f: F, delay_seconds: u64, tries: usize) -> Result<T, E>
+where
+    F: FnMut() -> Fut,
+    Fut: Future<Output = Result<T, E>>,
+{
+    let mut delay = delay_seconds;
+    for _ in 0..tries {
+        match f().await {
+            Ok(val) => return Ok(val),
+            Err(_) => {
+                sleep(Duration::from_secs(delay)).await;
+                delay *= 2;
+            }
+        }
+    }
+    f().await
+}
 
+async fn get_ip(client: &Client, host: &str) -> Result<String, Box<dyn std::error::Error>> {
+    let fetch_ip = || async {
+        let url = Url::parse(&format!("https://{}/ip.txt", host))?;
+        let response = client.get(url).send().await?;
         if response.status().is_success() {
-            let body = response.text().await?;
-            if let Ok(ip_addr) = body.parse::<Ipv4Addr>() {
+            if let Ok(ip_addr) = response.text().await?.parse::<Ipv4Addr>() {
                 return Ok(ip_addr.to_string());
             }
         }
+        Err("Failed to get a valid IP address".into())
+    };
+    retry(fetch_ip, RETRY_DELAY, 5).await
+}
 
-        tokio::time::sleep(Duration::from_secs(2u64.pow(i))).await;
-    }
+async fn generate_payload(client: &Client, host: &str, token: &str, key: &str) -> Result<Value, Box<dyn std::error::Error>> {
+    let (salta, saltb) = (random_hex(16), random_hex(16));
+    let wanip = get_ip(&client, host).await?;
+    let wandata_str = format!("{}{}{}{}{}", salta, token, wanip, saltb, key);
+    let hex_string = format!("{:x}", Sha512::digest(wandata_str.as_bytes()));
 
-    Err("Failed to get a valid IP address after 5 attempts".into())
+    Ok(serde_json::json!({
+        "status": "success",
+        "data": hex_string,
+        "additional": format!("{}{}", salta, saltb)
+    }))
 }
 
 #[tokio::main]
@@ -39,42 +67,21 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let token = env::var("TOKEN").expect("TOKEN environment variable not set");
     let hash = env::var("HASH").expect("HASH environment variable not set");
     let host = env::var("HOST").expect("HOST environment variable not set");
-    let endpoint = format!("https://{}/data/{}/", host, hash);
-    let sleep_duration: u64 = env::var("SLEEP_DURATION").unwrap_or("5".to_string()).parse().unwrap();
-    let sleep_duration = std::cmp::max(sleep_duration, 1) * 60;  // ensure minimum 1 minute, convert to seconds
+    let endpoint = Url::parse(&format!("https://{}/data/{}/", host, hash))?;
+    let sleep_duration: u64 = env::var("SLEEP_DURATION").unwrap_or_else(|_| "5".to_string()).parse().unwrap_or(1);
+    let sleep_duration = sleep_duration.max(1) * 60;
+
+    let client = Client::builder()
+        .user_agent("RustyIP")
+        .build()?;
 
     loop {
-        let client = reqwest::Client::builder()
-            .user_agent("RustyIP")
-            .build()?;
+        let data = generate_payload(&client, &host, &token, &key).await?;
 
-        let salta = random_hex(16);
-        let saltb = random_hex(16);
-        let wanip = get_ip(&client, &host).await?;
-        let salt = format!("{}{}", salta, saltb);
-        let wandata_str = format!("{}{}{}{}{}", salta, token, wanip, saltb, key);
-        let wandata = Sha512::digest(wandata_str.as_bytes());
-        let hex_string = format!("{:x}", wandata);
+        let params = [("payload", data.to_string())];
 
-        let data = json!({
-            "status": "success",
-            "data": hex_string,
-            "additional": salt
-        });
-
-        let mut params = HashMap::new();
-        params.insert("payload", data.to_string());
-
-        let mut delay = 2;
-        for _ in 0..3 {
-            let res = client.post(&endpoint).form(&params).send().await;  
-            if res.is_ok() && res.unwrap().status().is_success() {
-                break;
-            } else {
-                sleep(Duration::from_secs(delay)).await;
-                delay *= 2;  // exponential backoff
-            }
-        }
+        let send_with_retry = || client.post(endpoint.clone()).form(&params).send();
+        retry(send_with_retry, RETRY_DELAY, 3).await?;
 
         sleep(Duration::from_secs(sleep_duration)).await;
     }
